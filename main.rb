dd06250dd6448c50b5249ecbb5d78e8569cd4637
sharman <sharmanfaridz@gmail.com>
2024-08-15 18:36:54 +0800

    complete project

--- main.rb
+++ main.rb
@@ -25,36 +25,58 @@ end
 # p fibs_rec(8)
 
 def merge_sort(array)
-  return array if array.length == 1
+  return array if array.length <= 1
 
   mid_index = array.length / 2
   last_index = array.length - 1
 
-  puts "\n----- separator -----"
-  p array
-  puts "mid_index : #{mid_index}, last_index : #{last_index}"
+  # puts "\n----- separator -----"
+  # p array
+  # puts "mid_index : #{mid_index}, last_index : #{last_index}"
 
   sorted_left = merge_sort(array[0, mid_index])
   sorted_right = merge_sort(array[mid_index, last_index])
 
-  puts "sorted_left : #{sorted_left}, sorted_right : #{sorted_right}"
+  # puts "sorted_left : #{sorted_left}, sorted_right : #{sorted_right}"
 
-  if sorted_left.first > sorted_right.first
-    result = sorted_right + sorted_left
-  else
-    result = sorted_left + sorted_right
+  result = []
+  target_length = sorted_left.length + sorted_right.length
+
+  while result.length < target_length
+    left_min = sorted_left.min
+    right_min = sorted_right.min
+    left_empty = left_min.nil?
+    right_empty = right_min.nil?
+    both_not_empty = !left_empty && !right_empty
+    # puts "left : #{sorted_left}, right : #{sorted_right}"
+    # puts "left_min : #{left_min}, right_min : #{right_min}"
+    # puts "before result : #{result}\n\n"
+
+    if left_empty || (both_not_empty && right_min < left_min)
+      min_value = right_min
+      result << min_value
+      sorted_right.delete_at(sorted_right.index(min_value))
+      next
+
+    elsif right_empty || (both_not_empty && left_min <= right_min)
+      min_value = left_min
+      result << min_value
+      sorted_left.delete_at(sorted_left.index(min_value))
+      next
+    end
   end
-
-  sorted_left.each_with_index do |num, index|
-    
-  end
-
-  # if merge_sort(array[0, mid_index])
-  # result = (sorted_left + sorted_right).sort
-  puts "result : #{result}"
-
   result
 end
 
-# p merge_sort [5, 1, 2, 3, 6, 4]
+p merge_sort [5, 1, 2, 3, 6, 4]
 p merge_sort [7, 2, 5, 4, 1, 6, 0, 3]
+p merge_sort [1, 2, 3, 4, 5]
+p merge_sort [5, 4, 3, 2, 1]
+p merge_sort [3, 1, 4, 5, 2]
+p merge_sort [7, 7, 7, 7, 7]
+p merge_sort [15, 22, 15, 33, 8, 9, 22, 4, 7, 12, 7, 18, 2, 2, 19, 30]
+p merge_sort [3.14, 2.71, 0.577, 1.618, 4.669, 2.718]
+p merge_sort []
+p merge_sort [34, 7, 23, 32, 5, 62, 32, 43, 17, 10, 12, 13, 9, 44, 50, 31]
+p merge_sort [42]
+p merge_sort [-3, -1, -7, 4, 2, 0, -5, 8, -2]
